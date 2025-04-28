d89ed41aab2dff0f146410bbfdb3aeef13904214
ishak-abbaz <ishak.abbaz@univ-constantine2.dz>
2025-04-28 16:27:14 +0200

    Project completed

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,6 @@ import javax.swing.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main extends JPanel {
     public static void main(String[] args) {
-
-        try {
-            // Set system look and feel for the JOptionPane
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); // Set the pane to look like my system layout
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        DatabaseSetup ds = new DatabaseSetup();
-        ds.setupAndInsert("your pc database password");
-        // Uncomment after creating database
-//          AuthentifiactionWindow aw = new AuthentifiactionWindow();
+          AuthentifiactionWindow aw = new AuthentifiactionWindow();
     }
 }
